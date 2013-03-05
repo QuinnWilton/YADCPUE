11f10903dc74519f39a3164287e1e13c3fbf98e5
Shane Wilton <shane.wilton@gmail.com>
2013-03-05 00:51:09 -0800

    Start work on Memory.hs.
    I don't know what I'm doing.

--- src/Memory.hs
+++ src/Memory.hs
@@ -1,23 +1,29 @@
 module Memory where
 
 import Data.Word (Word16)
+import Data.Vector.Mutable as MV
+import Control.Monad.ST
 
-data Register = A | B | C | X | Y | Z | I | J
+data Register = A | B | C | X | Y | Z | I | J | SP | PC | EX | IA
     deriving (Bounded, Enum, Eq, Ord, Show)
 
 data Address
     = Register Register
-    | SP
-    | PC
-    | EX
-    | IA
     | Word Word16
     deriving (Eq)
 
 instance Show Address where
     show (Register r) = show r
-    show SP = "SP"
-    show PC = "PC"
-    show EX = "EX"
-    show IA = "IA"
     show (Word w) = "[" ++ show w ++ "]"
+
+type Memory s = (ST s (MV.MVector s Word16))
+
+fromAddress :: Address -> Int
+fromAddress (Word w) = fromIntegral w
+fromAddress (Register r) = 0x10001 + fromEnum r
+
+new :: Memory s
+new = do
+    memory <- MV.replicate (0x10001 + fromEnum (maxBound :: Register)) 0x0
+    write memory (fromEnum SP) 0xFFFF
+    return memory
