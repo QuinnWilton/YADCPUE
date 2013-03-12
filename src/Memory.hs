8c03bbde9f4df6521ab22f2b92602cf5dcd4b3f1
Shane Wilton <shane.wilton@gmail.com>
2013-03-12 01:34:58 -0700

    Oh my god. Memory.hs works I think.

--- src/Memory.hs
+++ src/Memory.hs
@@ -3,7 +3,6 @@ module Memory where
 import Data.Word (Word16)
 import Data.Vector.Mutable as MV
 import Control.Monad.ST
-import Control.Monad.Primitive
 
 data Register = A | B | C | X | Y | Z | I | J | SP | PC | EX | IA
     deriving (Bounded, Enum, Eq, Ord, Show)
@@ -26,11 +25,11 @@ fromAddress (Register r) = 0x10000 + fromEnum r
 new :: ST s (Memory s)
 new = do
     memory <- MV.replicate (0x10000 + fromEnum (maxBound :: Register)) 0x0
-    store memory (Register SP) 0xFFFF
+    store (Memory memory) (Register SP) 0xFFFF
     return $ Memory memory
 
 load :: Memory s -> Address -> ST s Word16
-load (Memory a) b = a >>= flip MV.read (fromAddress b)
+load (Memory a) b = MV.read a (fromAddress b)
 
 store :: Memory s -> Address -> Word16 -> ST s ()
-store (Memory a) b c = a >>= \x -> MV.write x (fromAddress b) c
+store (Memory a) b c = MV.write a (fromAddress b) c
